796eb10fc917b05779e4333431471cf5ab83a057
elizavetaosipova <evosipova_3@edu.hse.ru>
2024-01-04 16:12:45 +0300

    Initial Commit

---
+++ KsyCare/KsyCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  KsyCareApp.swift
+//  KsyCare
+//
+//  Created by Elizaveta Osipova on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct KsyCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
